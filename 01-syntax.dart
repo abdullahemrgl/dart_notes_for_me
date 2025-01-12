dbf4510597223e8b0c48f4fbfbca18ac34cd0de6
abdullahemrgl <abdullahemiroglu@gmail.com>
2025-01-12 23:23:46 +0300

    Edited

---
+++ 01-syntax.dart
@@ -0,0 +1,50 @@
+/// Documentation comment
+/// Go to [desp] function
+
+// import
+import 'dart:math';
+/*
+import 'package:flutter/flutter.dart';
+import '/path/to/file.dart' as | show | hide | deffered myFunc;
+*/
+
+// main
+void main(List<String> args) {
+
+    // variable & const
+    var world = 'World!';
+    Object objWorld = world; // with type
+    const pi = 3.1434354354;
+
+    // output
+    print('Hello, $world, ${args.length}');
+    
+    // input
+
+    // data type
+    Map<dynamic> myMap = {1: 'One'};
+    /*
+    num, double, int, bool, String, null, Object, dynamic, Never
+    List, Set, Map, Record
+    */
+        
+    
+    // operator
+    print(2 * 1 + 4 / 2 - 0);
+    /*
+    ~/, %, ??, .., ?.., ..., ...?, ? : , is, is!, as
+    */
+
+    // condation
+    
+
+    // loop
+
+    // class
+
+    // file operation
+
+}
+
+
+
